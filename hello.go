34e9f73a91385947d9a6e021c2ee7fbf2c21bedc
Frank Orthaus <32193361+clouseau142@users.noreply.github.com>
2017-11-08 22:37:26 +0100

    tests added

--- hello.go
+++ hello.go
@@ -3,7 +3,13 @@ package main
 import "fmt"
 
 func main() {
-    name := "Frank-2"
-    fmt.Printf("hello, world\n")
-    fmt.Printf(name)   
+	name := "Frank-2"
+	fmt.Printf("hello, world\n")
+	fmt.Printf(name + "\n")
+	fmt.Printf(Meinprint() + "\n")
+}
+
+// returns mein
+func Meinprint() string {
+	return "mein"
 }
---
+++ hello_test.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+import "testing"
+
+func TestTiming(t *testing.T) {
+	Meinprint()
+}
+
+func ExampleMeinprint() {
+	fmt.Printf(Meinprint())
+	// Output: mein
+}
